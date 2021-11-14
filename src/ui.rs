150a688e414f105e4b912860d27c0e2f82de3e8f
dfireBird <me@dfirebird.dev>
2021-11-14 17:32:30 +0530

    feat: implement terminal functions

--- src/ui.rs
+++ src/ui.rs
@@ -1,1 +1,28 @@
-mod events;
+pub mod events;
+
+use std::io::{self, Stdout};
+
+use anyhow::{Context, Result};
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{backend::CrosstermBackend, Terminal};
+
+pub fn initialize_term() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+    enable_raw_mode()?;
+    execute!(io::stdout(), EnterAlternateScreen, EnableMouseCapture)?;
+    Terminal::new(CrosstermBackend::new(io::stdout())).context("Terminal can't be initialized")
+}
+
+pub fn deinitialize_term(mut term: Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
+    disable_raw_mode()?;
+    execute!(
+        term.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    term.show_cursor()?;
+    Ok(())
+}
