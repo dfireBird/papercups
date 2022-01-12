177d4672f7f9c89a7a46a7d8735c625755778224
dfireBird <me@dfirebird.dev>
2022-01-12 23:48:10 +0530

    feat: add centered_rect widget function

--- src/ui/widgets.rs
+++ src/ui/widgets.rs
@@ -5,6 +5,7 @@ pub use dialog_box::{DialogBox, DialogState};
 use std::net::TcpStream;
 
 use tui::{
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
     widgets::{Block, Borders, List, ListItem, Paragraph},
@@ -52,4 +53,30 @@ pub fn connection_status_message(client: &Option<TcpStream>) -> Paragraph {
         ])
     };
     Paragraph::new(span)
+}
+
+pub fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Percentage(percent_y),
+                Constraint::Percentage((100 - percent_y) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_x) / 2),
+                Constraint::Percentage(percent_x),
+                Constraint::Percentage((100 - percent_x) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(popup_layout[1])[1]
 }
